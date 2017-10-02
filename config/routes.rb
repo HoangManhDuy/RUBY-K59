980e4c6f210dff16372f5f6830a35b5e2bd69180
Duyhm <hoangduyib96@gmail.com>
2017-10-02 17:39:53 +0700

    Generated static pages Exercises 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
