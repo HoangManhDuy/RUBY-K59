7998b4e9496fc9dbd6c7c2c9ddc7e1798061431d
Duyhm <hoangduyib96@gmail.com>
2017-12-14 22:39:18 +0700

    BT_tuan_12

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
 	attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   	validates :name,  presence: true, length: { maximum: 50 }
 
@@ -22,6 +31,21 @@ class User < ApplicationRecord
     	BCrypt::Password.create(string, cost: cost)
   	end
 
+    # Follows a user.
+    def follow(other_user)
+      following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
+    end
+
      private
 
     # Converts email to all lower-case.
@@ -34,4 +58,5 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
 end
